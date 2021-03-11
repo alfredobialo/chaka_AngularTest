c5c026a3641af60977821e07f126e271e1c5f1b2
Alfred Obialo <alfredcsdinc@gmail.com>
2021-03-11 15:28:53 +0100

    Create Required Stock Api To Alpha Vantage Api

--- scripts/modules/stockModule.js
+++ scripts/modules/stockModule.js
@@ -1,7 +1,34 @@
 (function (ng,window){
     ng.module("stockModule", ["stockApiModule"])
+        .directive("stockItem",["StockServicesApi", StockItemFunc])
+        .directive("stockItemList",["StockServicesApi", StockItemListFunc])
         ;
 
+    function StockItemFunc(StockServiceApi){
+        function ctl(scope, $state, $stateParams){
+            console.log($state, $stateParams);
+        }
+        return {
+            scope: {
+            },
+            template:"<div></div>",
+            controller: ["$scope","$state","$stateParams", ctl]
+        }
+    }
+    function StockItemListFunc(StockServiceApi){
+        function ctl(scope, $state, $stateParams){
+            console.log($state, $stateParams);
+            var defaultStockSymbol = ["BNGO","AAPL","INTC","IBM","GEVO","FCEL","SNAP","AAL","OCGN","PLTR","GE"];
+        }
 
-
+        return {
+            scope: {
+                title:"@?",
+                subTitle:"@?",
+                stockSymbol:"=?"
+            },
+            template:"<div></div>",
+            controller: ["$scope","$state","$stateParams", ctl]
+        }
+    }
 })(angular, window);
--- scripts/modules/stockModule_ApiService.js
+++ scripts/modules/stockModule_ApiService.js
@@ -1,7 +1,26 @@
-(function (ng,window){
+(function (ng, window) {
     ng.module("stockApiModule", [])
+        .factory("StockServicesApi", ["$http", StockServiceApi])
     ;
 
+    // Stock Api: base url and api Token are made available sin
+    function StockServiceApi($http) {
+        var baseUrl  = "https://www.alphavantage.co/query&function?=", token ="&apikey=R0VELXWZN90OIL1P";
 
+        function searchStock(symbol){
+            var url  = baseUrl+"SYMBOL_SEARCH&keywords="+symbol+token;
+            return $http.get(url);
+        }
+        function getStockDetail(symbol){
+            var url  = baseUrl+"OVERVIEW&symbol="+symbol+token;
+            return $http.get(url);
+        }
+        return {
+            searchSymbol : searchStock,
+            getStockDetail : getStockDetail,
+
+        }
+
+    }
 
 })(angular, window);
