372b28c075d7a8448f6c0c657e31097dd0cceee8
Alfred Obialo <alfredcsdinc@gmail.com>
2021-03-14 10:09:58 +0100

    Stock Symbol Overview Page: Binding value to the view

--- scripts/chakaApp.js
+++ scripts/chakaApp.js
@@ -1,6 +1,7 @@
 (function (ng, window) {
     ng.module("chakaApp", [ "ui.router", "appNavModule","stockModule"])
         .directive("myDirective", [MyDirectiveFunction])
+        .directive("progressBar", [ProgressBar])
         .run(["$state",RunApp])
       ;
 
@@ -10,12 +11,24 @@
             scope: {
                 data: "@?"
             },
-            template: "\n    <div class=\"row\">\n        <div class=\"col-xl-4\">\n            <div class=\"card card-custom bg-white gutter-b card-stretch shadow-sm\">\n                <div class=\"card-header bg-danger py-5\">\n                    <div class=\"card-title text-white\">Built with AngularJs 1.5</div>\n                </div>\n                <div class=\"card-body bg-white p-0 position-relative overflow-hidden\">\n                    <div class=\"p-6\">\n                        <div class=\'lead\'>\n                            By <span class=\'font-weight-bold text-danger lead\'>{{data }}</span> \n                            <br>\n                            Email: alfrdcsdinc@gmail.com\n                        </div>\n                    </div>\n                </div>\n            </div>\n\n\n        </div>\n\n    </div>\n"
+            template: "\n    <div class=\"row\">\n        <div class=\"col-xl-4\">\n            <div class=\"card card-custom bg-light gutter-b card-stretch \">\n                <div class=\"card-header bg-danger py-5\">\n                    <div class=\"card-title text-white\">Built with AngularJs 1.5</div>\n                </div>\n                <div class=\"card-body bg-white p-0 position-relative overflow-hidden\">\n                    <div class=\"p-6\">\n                        <div class=\'lead\'>\n                            By <span class=\'font-weight-bold text-danger lead\'>{{data }}</span> \n                            <br>\n                            Email: alfrdcsdinc@gmail.com\n                        </div>\n                    </div>\n                </div>\n            </div>\n\n\n        </div>\n\n    </div>\n"
         }
+    }
+    function ProgressBar() {
+        return  {
+            replace:true,
+            restrict:"E",
+            scope : {
+                processing  :"=?"
+            },
+            //language=Angular2HTML
+            template  : "<div ng-if=\'processing\' class=\"progressbar-header-container\">\n    <div class=\"asom-progress\">\n        <div class=\"indeterminate\"></div>\n    </div>\n</div>"
+        }
+
     }
 
     function RunApp($state){
-        $state.go("home");
+        //$state.go("home");
     }
 
 })(angular, window);
--- scripts/modules/stockModule.js
+++ scripts/modules/stockModule.js
@@ -1,63 +1,151 @@
 (function (ng, window) {
     ng.module("stockModule", ["stockApiModule"])
-        .controller("StockItemDetailController", ["$scope", "StockServicesApi", StockItemDetailController])
+        .controller("StockItemDetailController", ["$scope", "$state", "$stateParams", "StockServicesApi", StockItemDetailController])
         .directive("chakaStockItem", ["StockServicesApi", "$timeout", StockItemFunc])
         .directive("chakaStockItemDetail", [StockItemDetailDirectiveFunc])
         .directive("chakaStockItemList", ["StockServicesApi", StockItemListFunc])
     ;
-    function StockItemDetailController(scope, StockServiceApi){
+
+    function StockItemDetailController(scope, $state, $stateParams, StockServiceApi) {
+        scope.param = $stateParams;
+        scope.data = {};
+        scope.loading = false;
+
+        console.log($stateParams, "State Params")
+
+        function init() {
+            getSymbolOverview();
+        }
+
+        init();
+
+        function getSymbolOverview() {
+            scope.loading = true;
+            StockServiceApi.getStockDetail(scope.param.symbol)
+                .then(function (response) {
+                    console.log(response);
+                    scope.loading = false;
+                    if (ng.isDefined(response.data.Symbol)) {
+                        transformData(response.data);
+                        scope.hasError = false;
+                    } else if (ng.isDefined(response.data.Note)) {
+                        scope.hasError = true;
+                        scope.errMessage = "Due to Api Request Limitation, your request was aborted"
+                    }
+                }, function (errResponse) {
+                    console.log(errResponse);
+                })
+        }
+
+        function transformData(data) {
+            scope.data = {
+                symbol: data.Symbol,
+                assetType: data.AssetType,
+                name: data.Name,
+                description: data.Description,
+                address: data.Address,
+                exchange: data.Exchange,
+                currency: data.Currency,
+                country: data.Country,
+                sector: data.Technology,
+                industry: data.Industry,
+                noOfEmployee: _.toNumber(data.FullTimeEmployees),
+                outstandingShares: _.toNumber(data.SharesOutstanding),
+                sharesFloat: _.toNumber(data.SharesFloat),
+                exDividendDate: (data.ExDividendDate),
+                weekRange52: data["52WeekLow"] + " - " + data["52WeekHigh"],
+                dividendYield: data.DividendYield,
+                dividendPerShare: data.DividendPerShare,
+                marketCap: _.toNumber(data.MarketCapitalization),
+                eps: data.EPS,
+                peRatio: data.PERatio,
+                beta: data.Beta,
+            };
+            console.log(scope.data);
+        }
 
     }
-    function StockItemDetailDirectiveFunc(){
+
+    function StockItemDetailDirectiveFunc() {
         return {
-            scope :{},
-            template: "",
-            replace: true
+            scope: {},
+            template: "<div class=\'bg-white px-6 px-lg-10 my-10 my-lg-15\'>\n    <div class=\"\">\n        <h4 class=\"font-weight-bolder  \" style=\'font-size: 1.9rem;\'>{{data.name}} ({{data.symbol}})</h4>\n        <h1 class=\"font-weight-bolder \" style=\'font-size: 4.6rem;\'>{{param.prevClose}}\n            <span class=\"font-size-h6-xxl font-size-h6-sm\">{{param.pos}}</span>\n            <span class=\"font-size-h6-xxl font-size-h6-sm text-danger\">({{param.neg}})</span>\n        </h1>\n        <div class=\'mt-n3\'>\n            <span class=\"font-size-sm text-dark-65\">IEX real time price as of [3:15:01 PM].</span>\n        </div>\n        <div class=\"mt-10\">\n            <div class=\"row\">\n                <div class=\"col-xl-12\">\n                    <div class=\"chart-container\">\n\n                    </div>\n                    <div class=\"chart-container-bar-chart\">\n\n                    </div>\n                    <div class=\"chart-time-series d-flex justify-content-start align-items-center\">\n                        <a href=\"\" class=\"py-4 px-4 bg-light\">1D</a>\n                        <a href=\"\" class=\"py-4 px-4 bg-light\">1M</a>\n                        <a href=\"\" class=\"py-4 px-4 bg-light\">3M</a>\n                        <a href=\"\" class=\"py-4 px-4 bg-light\">6M</a>\n                        <a href=\"\" class=\"py-4 px-4 bg-light\">YTD</a>\n                        <a href=\"\" class=\"py-4 px-4 bg-light\">1Y</a>\n                        <a href=\"\" class=\"py-4 px-4 bg-light\">2Y</a>\n                        <a href=\"\" class=\"py-4 px-4 bg-light\">5Y</a>\n                    </div>\n                </div>\n            </div>\n        </div>\n        <div class=\"mt-7\">\n\n            <div class=\"row\">\n                <div class=\"col-6\">\n                    <h3>Profile</h3>\n                    <div \n                         ng-class=\'{\"expand-content\":showMore, \"contract-content\":!showMore}\'\n                         class=\'d-flex flex-column justify-content-between align-items-start mb-2\'>\n                        <p class=\"lead\" ng-bind=\"data.description\"></p>\n                    </div>\n                    <button class=\'btn btn-outline-danger btn-xs\' ng-click=\'showMore = !showMore\'>\n                        <span ng-show=\'!showMore\'>Show More</span>\n                        <span ng-show=\'showMore\'>Show Less</span>\n                    </button>\n                   \n                    <hr>\n                    <div class=\'row\'>\n                        <div class=\"col-md-6 col-12\">\n                            <span class=\"text-uppercase\">Exchange</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.exchange\">---</p>\n                            <span class=\"text-uppercase\">Float</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.sharesFloat\">---</p>\n                            <span class=\"text-uppercase\">Sector</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.sector\">---</p>\n                            <span class=\"text-uppercase\">Address</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.address\">--</p>\n\n                        </div>\n                        <div class=\"col-md-6 col-12\">\n                            <span class=\"text-uppercase\">Number Of Employees</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.noOfEmployee\">---</p>\n                            <span class=\"text-uppercase\">Shares Outstanding</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.outstandingShares\">--</p>\n                            <span class=\"text-uppercase\">Industry</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.industry\">--</p>\n\n                        </div>  \n                    </div>\n                    \n                </div>\n                <div class=\"col-6\">\n                    <hr>\n                    <div class=\"row\">\n                        <div class=\"col-md-4 col-12\">\n                            <span class=\"text-uppercase\">Volumn</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"param.volume\">--</p>\n                            <span class=\"text-uppercase\" >52 Week Range</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.weekRange52\">--</p>\n                            <span class=\"text-uppercase\">Beta</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.beta\">--</p>\n                            <span class=\"text-uppercase\">Dividend & Yield</span>\n                            <p class=\"font-weight-bold mb-7\" ng-bind=\"data.dividendYield\">--</p>\n\n                        </div>\n                        <div class=\"col-md-4 col-12\"><span class=\"text-uppercase\">AV</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                            <span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                            <span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                            <span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                            <span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                        </div>\n                        <div class=\"col-md-4 col-12\"><span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                            <span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                            <span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                            <span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                            <span class=\"text-uppercase\">volumn</span>\n                            <p class=\"font-weight-bold mb-7\">00000</p>\n                        </div>\n                    </div>\n                </div>\n            </div>\n\n        </div>\n\n    </div>\n</div>",
+            replace: true,
+            controller: "StockItemDetailController"
         }
     }
 
     function StockItemFunc(StockServiceApi, $timeout) {
         var queue = [];
+        var counter = 0;
+
         function ctl(scope, $state, $stateParams) {
 
             scope.stock = {};
             scope.loading = false;
-
-            scope.getStockOverview = getStockOverview;
-
-            function getStockOverview(symbol) {
+            scope.failed = false;
+            scope.gotoToStockOverviewPage = gotoToStockOverviewPage;
+            queue.push(scope.symbol);
+            console.log(queue);
+            function gotoToStockOverviewPage(symbol) {
                 if (!scope.loading) {
-                    $state.go("stock", {symbol: symbol});
-                    console.log($stateParams, $state);
+                    var params = {
+                        symbol: symbol,
+                        pos: scope.stock.pos,
+                        neg: scope.stock.neg,
+                        vol: scope.stock.volume,
+                        prevClose: scope.stock.prevClose
+                    };
+                    $state.go("home.stock", params);
+
                 }
             }
 
-            //timer();
+            /*
+            Due to Alpha Vantage Api Request Limitation for Demo Account, I have to batch some request to the server
+            This will be better when abstracted to a seperate service
+            *
+            * */
+           // timer();
+
             function timer() {
                 // batch five Symbol
-                $timeout(getStockInfo, 3000);
+                if (counter >= queue.length) {
+                    queue =[];
+                    counter = 0;
+                    return;
+                }
+                window.setTimeout(
+                    function () {
+                        console.log("Counter Is= " , counter);
+                        getStockInfo(queue[counter])
+                    }, 3000);
             }
 
-            getStockInfo();
-
-
+            getStockInfo()
             function getStockInfo() {
+                var symbol = scope.symbol;
                 scope.loading = true;
-                queue.push(scope.symbol);
-                console.log(queue);
+                console.log(symbol, counter,"counter");
                 //return;
-                StockServiceApi.searchSymbol(scope.symbol)
+                StockServiceApi.searchSymbol(symbol)
                     .then(function (response) {
                         scope.loading = false;
-                        console.log(response.data);
+                       // console.log(response.data);
                         if (ng.isDefined(response.data["Global Quote"])) {
                             scope.stock.pos = response.data["Global Quote"]["02. open"];
                             scope.stock.neg = response.data["Global Quote"]["10. change percent"];
-
+                            scope.stock.prevClose = response.data["Global Quote"]["08. previous close"];
+                            scope.stock.volume = response.data["Global Quote"]["06. volume"];
+                            scope.failed = false;
+                        } else {
+                            scope.failed = true;
                         }
-                        //timer();
+                      /*  counter += 1;
+                        timer();*/
                     }, function (failedResponse) {
                         scope.loading = false;
+                        scope.failed = true;
                         console.log(failedResponse, "failed request");
                     })
             }
@@ -68,7 +156,7 @@
             scope: {
                 symbol: "=?"
             },
-            template: "<div class=\"stock-item mr-3 mb-3 p-3 d-flex flex-column flex-grow-1 justify-content-between align-items-start\"\n     ng-click=\'getStockOverview(symbol)\'>\n    <div>\n        <h5 class=\'stock-header mb-n1\'>{{symbol}}</h5>\n        <div ng-if=\'loading\' class=\'d-flex flex-column mt-2 pt-2 justify-content-center align-items-center w-100\'>\n            <div  class=\"spinner spinner-success spinner-border-sm\">\n                \n            </div>\n        </div>\n        <span class=\'stock-name\' style=\'font-size: 0.78rem\'>{{stock.companyName}}</span>\n    </div>\n    <div class=\"d-flex flex-column justify-content-end align-items-end pr-3 w-100\">\n        <span class=\"valuation\">{{stock.pos}}</span>\n        <span class=\"valuation-pt\">{{stock.neg}}</span>\n    </div>\n</div>",
+            template: "<div class=\"stock-item mr-3 mb-3 p-3 d-flex flex-column flex-grow-1 justify-content-between align-items-start\"\n     ng-click=\'gotoToStockOverviewPage(symbol)\' ng-class=\'{\"border-2 border-danger\": failed}\'>\n    <div>\n        <h5 class=\'stock-header mb-n1\'>{{symbol}}</h5>\n        <div ng-if=\'loading\' class=\'d-flex flex-column mt-2 pt-2 justify-content-center align-items-center w-100\'>\n            <div  class=\"spinner spinner-success spinner-border-sm\">\n                \n            </div>\n        </div>\n        <span class=\'stock-name\' style=\'font-size: 0.78rem\'>{{stock.companyName}}</span>\n    </div>\n    <div class=\"d-flex flex-column justify-content-end align-items-end pr-3 w-100\">\n        <span class=\'text-danger\' ng-if=\'failed\'>Request failed!</span>\n        <span class=\"valuation\">{{stock.pos}}</span>\n        <span class=\"valuation-pt\">{{stock.neg}}</span>\n    </div>\n</div>",
             controller: ["$scope", "$state", "$stateParams", ctl]
         }
     }
